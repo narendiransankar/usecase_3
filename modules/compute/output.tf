5acb93458d1debc67f8a8b26c418318388911fb4
narendiransankar <narendiransankar1699@gmail.com>
2025-07-12 12:58:32 +0530

    Update output.tf

--- modules/compute/output.tf
+++ modules/compute/output.tf
@@ -1,7 +1,8 @@
-output "web_instance_ids" {
-  value = aws_instance.web[*].id
+output "instance_a_id" {
+  value = aws_instance.web.id
 }
 
 output "web_public_ips" {
   value = aws_instance.web[*].public_ip
 }
+

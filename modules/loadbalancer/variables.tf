4412591449b6fb0cfeec673302a515a6d4b80862
narendiransankar <narendiransankar1699@gmail.com>
2025-07-12 12:46:08 +0530

    Create variables.tf

---
+++ modules/loadbalancer/variables.tf
@@ -0,0 +1,17 @@
+variable "subnet_ids" {
+  description = "List of subnet IDs for ALB"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID for ALB"
+  type        = string
+}
+variable "instance_a_id" {
+  description = "instance a id"
+}
+
+variable "alb_security_group_id" {
+  description = "iindtance sg id"
+}
+

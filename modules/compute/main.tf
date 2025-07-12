dc900608a48860a30b57b73716222d7c8a47cd5f
narendiransankar <narendiransankar1699@gmail.com>
2025-07-12 12:24:33 +0530

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -4,17 +4,11 @@ resource "aws_instance" "web" {
   ami           = "ami-020cba7c55df1f615"
   instance_type = var.instance_type
   subnet_id     = var.public_subnet_ids[count.index]
+  associate_public_ip_address = true
   vpc_security_group_ids = [var.web_sg_id]
   key_name      = var.key_name
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y apache2
-              systemctl start apache2
-              systemctl enable apache2
-              echo "<h1>Hello from ${var.env} Web Server ${count.index + 1}</h1>" > /var/www/html/index.html
-              EOF
+user_data = file("${path.module}/userdata-0.sh")
 
   tags = {
     Name = "${var.env}-web-${count.index}"

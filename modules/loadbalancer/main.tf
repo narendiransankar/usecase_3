d794bc6031218afc16d1195e89357154b980e558
narendiransankar <narendiransankar1699@gmail.com>
2025-07-12 13:44:10 +0530

    Update main.tf

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -32,53 +32,6 @@ resource "aws_lb_target_group_attachment" "tg_homepage_attachment" {
  
 }
 
-resource "aws_lb_target_group" "tg_images" {
-  name        = "images-tg"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = var.vpc_id
-
-  health_check {
-    path                = "/images/index.html"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_target_group_attachment" "tg_images_attachment" {
-  target_group_arn = aws_lb_target_group.tg_images.arn
-  target_id        = var.instance_b_id
-  port             = 80
- 
-}
-
-
-
-
-resource "aws_lb_target_group" "tg_register" {
-  name        = "register-tg"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = var.vpc_id
-
-  health_check {
-    path                = "/register/index.html"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-resource "aws_lb_target_group_attachment" "tg_register_attachment" {
-  target_group_arn = aws_lb_target_group.tg_register.arn
-  target_id        = var.instance_c_id
-  port             = 80
-
-}
 
 resource "aws_lb_listener" "listener_http" {
   load_balancer_arn = aws_lb.alb.arn
@@ -91,35 +44,7 @@ resource "aws_lb_listener" "listener_http" {
   }
 }
 
-resource "aws_lb_listener_rule" "rule_images" {
-  listener_arn = aws_lb_listener.listener_http.arn
 
-  condition {
-    path_pattern {
-      values = ["/images/*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg_images.arn
-  }
-}
-
-resource "aws_lb_listener_rule" "rule_register" {
-  listener_arn = aws_lb_listener.listener_http.arn
-
-  condition {
-    path_pattern {
-      values = ["/register/*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg_register.arn
-  }
-}
 
 resource "aws_security_group" "alb_sg" {
   name        = "alb-sg"

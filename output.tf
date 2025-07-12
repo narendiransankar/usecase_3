123d098c16c8b547e45f16f3455b7d5353ae3205
narendiransankar <narendiransankar1699@gmail.com>
2025-07-12 12:39:17 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,12 +3,6 @@ output "web_public_ips" {
   value       = module.compute.web_public_ips
 }
 
-output "db_endpoint" {
-  description = "RDS endpoint"
-  value       = module.database.db_endpoint
-  sensitive   = true
-}
-
 output "vpc_id" {
   description = "VPC ID"
   value       = module.network.vpc_id
